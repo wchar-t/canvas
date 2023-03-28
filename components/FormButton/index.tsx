c3ef7942def3a2ff07242ebc5c4c007be5b30816
joao <joaopedro99_stylle@hotmail.com>
2023-03-28 09:54:29 -0300

    Settings page UI done. Needs API

--- components/FormButton/index.tsx
+++ components/FormButton/index.tsx
@@ -1,13 +1,14 @@
 import styles from '../../styles/components/FormButton.module.css';
 
 interface FormButtonOptions {
-  label: string,
+  label: string | JSX.Element,
+  theme?: 'tertiary',
   onClick: () => void,
 }
 
-export default function FormButton({ label, onClick }: FormButtonOptions) {
+export default function FormButton({ label, theme, onClick }: FormButtonOptions) {
   return (
-    <button type="button" className={styles.button} onClick={onClick}>
+    <button type="button" className={`${styles.button} ${theme === 'tertiary' ? styles.tertiary : undefined}`} onClick={onClick}>
       {label}
     </button>
   );
--- components/SideSettingsMenu/index.tsx
+++ components/SideSettingsMenu/index.tsx
@@ -5,6 +5,7 @@ import styles from '../../styles/components/SideSettingsMenu.module.css';
 import TextInput from '../TextInput';
 import { Session } from '../../interfaces/shared/session';
 import { onMenuItemClick } from '../../controllers/Settings';
+import FormButton from '../FormButton';
 
 export default function SideSettingsMenu({ session }: { session: Session }) {
   return (
@@ -19,13 +20,16 @@ export default function SideSettingsMenu({ session }: { session: Session }) {
         <Content identifier="public" title="Perfil público" isActive>
           <TextInput label="Nome" name="name" defaultValue={session.profile.name} placeholder="Seu nome" />
           <TextInput label="Bio" name="bio" defaultValue={session.profile.bio} placeholder="Digite uma bio aqui" isMultiline />
+          <FormButton label="Salvar" onClick={() => {}} theme="tertiary" />
         </Content>
-        <Content identifier="security" title="Mudar email">
-          <TextInput label="email" name="Email" defaultValue={session.email} placeholder="Seu email" />
+        <Content identifier="security" title="Mudar email" isRow>
+          <TextInput label="Email" name="email" defaultValue={session.email} placeholder="Seu email" />
+          <FormButton label={<Icon name="floppy-disk" />} onClick={() => {}} theme="tertiary" />
         </Content>
         <Content identifier="security" title="Mudar Senha">
           <TextInput label="Senha" name="password" placeholder="Sua senha antiga" isPassword />
           <TextInput label="Senha" name="newpassword" placeholder="Sua senha nova" isPassword />
+          <FormButton label="Salvar" onClick={() => {}} theme="tertiary" />
         </Content>
       </div>
     </div>

b1f76fdea1ef0bcda35fd8e913955f3f533c8de8
Emanuel Montenegro <emanuel.montenegro@mail.uft.edu.br>
2023-03-28 17:59:47 -0300

    meu commit

--- controllers/Login.ts
+++ controllers/Login.ts
@@ -31,6 +31,29 @@ export async function login(
   localStorage.setItem('token', result.jwt);
 }
 
-export async function register() {
-  // todo
+export async function register(
+  nameRef: RefObject<HTMLInputElement>,
+  usernameRef: RefObject<HTMLInputElement>,
+  emailRef: RefObject<HTMLInputElement>,
+  passwordRef: RefObject<HTMLInputElement>
+) {
+  if (!usernameRef.current || !passwordRef.current || !emailRef.current || !nameRef.current) return;
+
+  const username = usernameRef.current.value.toLowerCase().trim();
+  const password = passwordRef.current.value.trim();
+  const name = nameRef.current.value.toLowerCase().trim();
+  const email = emailRef.current.value.trim();
+
+  const { error, result } = await Api.register(name, username, email, password);
+
+  if (error) {
+    if (error.code === 'user_already_exists') {
+      usernameRef.current.classList.add(styles.error);
+    }
+    return;
+  }
+
+  usernameRef.current.classList.remove(styles.error);
+
+  localStorage.setItem('token', result.jwt);
 }
